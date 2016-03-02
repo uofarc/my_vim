818e1a608cc3c7c135b1e94e25b1ced12f77d5f6
cain_mbp <cain@cain-mbpdeMacBook-Pro.local>
2016-03-02 17:23:46 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 #git clone https://github.com/uofarc/my_vim.git
-sudo apt-get install zsh
-sudo apt-get install vim
-sudo apt-get install git
+#sudo apt-get install zsh
+#sudo apt-get install vim
+#sudo apt-get install git
 #sudo raspi-config
+
+brew install zsh
+brew install vim
+brew install git
 sudo chsh -s /bin/zsh cain
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
