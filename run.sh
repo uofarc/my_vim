aa5ba72135edf1019845cc3e07610681244f9c03
cain_mbp <cain@cain-mbpdeMacBook-Pro.local>
2016-03-03 01:40:46 +0800

    fix folder

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ cp _ctags ~/.ctags
 mkdir -p  ~/.vim
 cp vimrc.vim ~/.vim/
 cp bundles.vim ~/.vim/
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle
 vim +PluginInstall +qall
