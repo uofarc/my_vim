0dcd6809fca3871c98efe6ddf98038a87f9461cd
uofa <uofa@qq.com>
2017-03-13 11:01:15 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@
 #sudo apt-get install zsh
 #sudo apt-get install vim
 #sudo apt-get install git
+#sudo apt-get install tmux
 #sudo raspi-config
 
 brew install zsh
--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@ cp _ctags ~/.ctags
 mkdir -p  ~/.vim
 cp vimrc.vim ~/.vim/
 cp bundles.vim ~/.vim/
+cp tmux.txt ~/.tmux.conf
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle
 vim +PluginInstall +qall
