f1434a138ade56f9321ede275f056aabc253e122
uofa <uofa@qq.com>
2017-12-25 16:08:50 +0800

    udpate

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #git clone https://github.com/uofarc/my_vim.git
-#sudo apt-get install zsh
-#sudo apt-get install vim
-#sudo apt-get install git
-#sudo apt-get install tmux
+sudo apt-get install zsh
+sudo apt-get install vim
+sudo apt-get install git
+sudo apt-get install tmux
 #sudo raspi-config
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install zsh
-brew install vim
-brew install git
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#brew install zsh
+#brew install vim
+#brew install git
 owner=`who am i | awk '{print $1}'`
-sudo chsh -s /bin/zsh $user
+sudo chsh -s /bin/zsh $owner
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
